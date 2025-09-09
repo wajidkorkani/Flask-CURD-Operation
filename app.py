4649c54decddc6fea9c9858fe6d2ac5dd73d494e
Wajid <errormrcoder@example.com>
2025-09-09 09:27:43 +0500

    Add Update and Delete user.

--- app.py
+++ app.py
@@ -43,5 +43,35 @@ def list_users():
     users = User.query.all()
     return render('users.html', users=users)
 
+@app.route('/delete-user-<int:id>')
+def delete_user(id):
+    try:
+        user_to_delete = User.query.get_or_404(id)
+        db.session.delete(user_to_delete)
+        db.session.commit()
+        return redirect(url_for('list_users'))
+    except:
+        return redirect(url_for('list_users'))
+    
+
+@app.route('/update-user/form-<int:id>')
+def Update_user_form(id):
+    user = User.query.get_or_404(id)
+    return render('update_user.html', user=user)
+
+@app.route('/update-user', methods=['POST'])
+def update_user_info():
+    if request.method == 'POST':
+        id = request.form.get('id')
+        user = User.query.get_or_404(id)
+        username = request.form.get('username')
+        name = request.form.get('name')
+        if username and name:
+            user.username = username
+            user.name = name
+            db.session.commit()
+            return redirect(url_for('list_users'))
+    return render('update_user.html', user=user)
+
 if __name__ == '__main__':
     app.run(debug=True)
